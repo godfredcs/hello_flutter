bf991c7bd6e6934bff29a4be36e436b8f3b54827
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2018-07-02 12:23:35 +0000

    cut and pasted welcome class into a welcome file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "./ui/welcome_home.dart";
 
 void main() {
   runApp(
@@ -7,26 +8,4 @@ void main() {
       home: new Welcome()
     )
   );
-}
-
-class Welcome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      color: Colors.deepPurple,
-      child: new Center(
-        child: new Text(
-          "Welcome to Flutter",
-          textDirection: TextDirection.ltr,
-          style: new TextStyle(
-            fontWeight: FontWeight.w800,
-            fontStyle: FontStyle.italic,
-            fontSize: 34.0,
-            color: Colors.white
-          ),
-        )
-      ),
-    );
-  }
-
 }
