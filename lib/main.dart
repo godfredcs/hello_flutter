bc4edc258e68a7eeaed6189c78057cda93c406de
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2018-07-02 12:14:58 +0000

    Created Welcome class that extends StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,31 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    new Material(
-      color: Colors.amberAccent,
-
-      child: new Center(
-        child: new Text(
-          "Hello World",
-          textDirection: TextDirection.ltr,
-          style: new TextStyle(fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
-        ),
-      ),
+    new MaterialApp(
+      title: "Welcome Screen",
+      home: new Welcome()
     )
   );
+}
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.deepPurple,
+      child: new Center(
+        child: new Text(
+          "Welcome to Flutter",
+          textDirection: TextDirection.ltr,
+          style: new TextStyle(
+            fontWeight: FontWeight.w800,
+            fontStyle: FontStyle.italic,
+            fontSize: 34.0,
+            color: Colors.white
+          ),
+        )
+      ),
+    );
+  }
+
 }
