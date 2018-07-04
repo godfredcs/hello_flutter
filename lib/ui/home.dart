ada45f6e9ba3aae1d28255ec93e2359ea0efe7e8
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2018-07-04 22:20:47 +0000

    Learned about row, column, and stack

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -11,7 +11,19 @@ class Home extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
 
         children: <Widget>[
-          new Text("First item", textDirection: TextDirection.ltr, style: new TextStyle(color: Colors.white, fontSize: 18.0)),
+          new Container(
+            child: new Row(
+              children: <Widget>[
+                new Text("Row 1", textDirection: TextDirection.ltr, style: new TextStyle(color: Colors.lightBlue, fontSize: 12.9)),
+
+                new Text("Row 2", textDirection: TextDirection.ltr, style: new TextStyle(color: Colors.cyan, fontSize: 12.9)),
+
+                const Expanded(
+                  child: const Text("Row 3"),
+                )
+              ],
+            ),
+          ),
 
           new Text("Second item", textDirection: TextDirection.ltr, style: new TextStyle(color: Colors.blue, fontSize: 18.0)),
 
@@ -19,6 +31,15 @@ class Home extends StatelessWidget {
             color: Colors.deepOrange.shade100,
             alignment: Alignment.bottomLeft,
             child: new Text("Third item", textDirection: TextDirection.ltr, style: new TextStyle(color: Colors.blue, fontSize: 18.0))
+          ),
+
+          new Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              const Text("Hello world"),
+              const Text("Hi World"),
+              const Text("Are you there world")
+            ],
           )
         ],
       )
